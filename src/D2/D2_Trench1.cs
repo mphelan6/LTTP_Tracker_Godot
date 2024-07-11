cf1dc44a56af141b12113de9a44f7dc40c8406e3
Mike <mikep359@gmail.com>
2024-07-10 20:36:40 -0700

    D2 progress

---
+++ src/D2/D2_Trench1.cs
@@ -0,0 +1,8 @@
+public partial class D2_Trench1 : CheckSprite
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		StartTracking((int)Check.Name.SWAMP_PALACE_TRENCH_1_POT_KEY);
+	}
+}
