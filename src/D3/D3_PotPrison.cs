e58bd81948e217278c0dd6eca26d951b03b148ae
Mike <mikep359@gmail.com>
2024-07-11 19:34:34 -0700

    D3 dungeon complete

--- src/D3/D3_PotPrison.cs
+++ src/D3/D3_PotPrison.cs
@@ -3,6 +3,6 @@ public partial class D3_PotPrison : CheckSprite
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		StartTracking((int)Check.Name.LINKS_HOUSE);
+		StartTracking((int)Check.Name.SKULL_WOODS_POT_PRISON);
 	}
 }
